4f38cf6b6787fb8a7155a3a1961beb2d83ec7c4d
Rubèn Suet <suet@mediamarktsaturn.com>
2021-02-28 21:28:50 +0100

    00 - First commit hello world

---
+++ src/main/java/firstRestful/Ruben/RubenApplication.java
@@ -0,0 +1,13 @@
+package firstRestful.Ruben;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/firstRestful/Ruben/RubenApplicationTests.java
@@ -0,0 +1,13 @@
+package firstRestful.Ruben;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
